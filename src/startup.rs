5888c711c23ccfaaa94b7553add156cbc75de1ac
Daniel-P-Lima <limadanielpereira@gmail.com>
2026-03-27 20:40:59 -0300

    add logging

--- src/startup.rs
+++ src/startup.rs
@@ -1,13 +1,16 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
-use actix_web::{App, HttpServer, web};
+use actix_web::{App, HttpServer, web, middleware::Logger};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use env_logger::Env;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let connection = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("subscriptions", web::post().to(subscribe))
             // Register the connection as part of the application state
