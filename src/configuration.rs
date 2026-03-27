dc9af0b6ffdd4ebe001b86ae87181b0fe0ac70ac
Daniel-P-Lima <limadanielpereira@gmail.com>
2026-03-27 16:35:19 -0300

    rewrite tests; rewrite connection with postgre to use pooling; create insert and select api routes;

--- src/configuration.rs
+++ src/configuration.rs
@@ -17,10 +17,18 @@ impl DatabaseSettings {
     pub fn connection_string(&self) -> String {
         format!(
             "postgres://{}:{}@{}:{}/{}",
-            self.username, self.password, self.host, self.port, self.database_name 
+            self.username, self.password, self.host, self.port, self.database_name
+        )
+    }
+
+    pub fn connection_string_without_db(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}",
+            self.username, self.password, self.host, self.port
         )
     }
 }
+
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     let settings = config::Config::builder()
         .add_source(config::File::with_name("configuration"))
